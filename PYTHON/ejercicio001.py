1c9adda99aeef9c82b2b5438e376fe62388376d5
Cristhian <cristhiancg5@outlook.com>
2021-06-02 21:57:03 -0500

    mi primer commit

---
+++ PYTHON/ejercicio001.py
@@ -0,0 +1,16 @@
+print("Hola mundo")
+
+nombre = 'Cristhian Guin'
+edad = 25 
+genero = 'masculino'
+telefono = '0969785871'
+print(f'Mi nombre es :   {nombre}  y mi edad es {edad}  y mi genero es {genero} y su telefono es {telefono} ' )
+# Este es un comentario
+
+
+primer_numero = input('ingrese el primer numero: \t')
+segundo_numero = input('ingrese el segundo numero: \t')
+resultado = int(primer_numero) + float(segundo_numero)
+
+print(resultado)
+print(type(resultado))
---
+++ PYTHON/ejercicio002.py
@@ -0,0 +1,6 @@
+#calcular el area y el perimetro del cuadrado 
+lado = input('Ingrese los lados del cuadrado: \t')
+perimetro = int(lado)*4
+print(f'El perimetro del cuadrado es:  {perimetro}')
+area = int(lado) * int(lado)
+print(f'El area del cuadrado es: {area}')
