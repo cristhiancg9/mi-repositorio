9b180a32401716f0ea6a3e48160277e025ed582e
Cristhian <cristhiancg5@outlook.com>
2021-06-03 20:27:14 -0500

    Ejercicio005 condicionales

--- PYTHON/ejercicio003.py
+++ PYTHON/ejercicio003.py
@@ -1,1 +1,6 @@
 #calcular el area y el perimetro del triangulo 
+base = float(input("ingresa la base del triangulo: \t"))
+altura = float(input("ingresa la altura del triangulo: \t"))
+
+print(f"El resultado del area del triangulo es {((base * altura)/2)}")
+print(f"El resultado de la altura del triangulo es {(base * 3)}")
---
+++ PYTHON/ejercicio005.py
@@ -0,0 +1,8 @@
+#edad
+edad = int(input("Ingrese la edad: \t"))
+if edad > 64:
+    print("TERCERA EDAD")
+elif edad > 17:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
