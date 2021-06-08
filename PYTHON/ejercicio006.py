b4e3360d460fb4ed0b65bd6c8ff3c82624620893
Cristhian <cristhiancg5@outlook.com>
2021-06-07 21:47:30 -0500

    ejercicio007

---
+++ PYTHON/ejercicio006.py
@@ -0,0 +1,10 @@
+#un ejercicio que igrese un número y que muestre si es par o impar 
+num = int(input("ingrese un número: \t"))
+if num == 0: 
+    print("neutro")
+elif num % 2 == 0:
+    print("es par")
+else :
+    print("es impar")
+
+
---
+++ PYTHON/ejercicio007.py
@@ -0,0 +1,20 @@
+#ejercicio operadores matematicos
+numero1 = 5
+numer02 = 2
+
+exponente = 5 ** 2 #25
+suma = 5 + 2 #7
+resta = 5 - 2 #3
+multiplicacion = 5 * 2 #10 
+division = 5 / 2 #2,5
+division_entera = 5//2 #2
+modulo = 5 % 2 #1
+
+print(f"el resultado del exponente es {exponente}")
+print(f"el resultado de la suma  es {suma}")
+print(f"el resultado de la resta es {resta}")
+print(f"el resultado de la multiplicacion es {multiplicacion}")
+print(f"el resultado de la divison es {division}")
+print(f"el resultado de la divison entera es {division_entera}")
+print(f"el resultado del MOD es {modulo}")
+
